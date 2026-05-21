4dbc9418824801a84d2cd5e2d4266037b891febf
muchirisworld <waburimuchiri@gmail.com>
2026-05-21 05:26:25 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -40,16 +40,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = Client::with_config(config);
 
+    let msgs: Vec<Message> = vec![Message{
+        role: "user".to_string(),
+        content: Some(args.prompt),
+        tool_calls: None
+    }];
+
+    let mut resp: LLMResponse;
+    let mut tool_calls: Vec<ToolCall> = Vec::new();
+    
+    loop { // <- START
+        
     #[allow(unused_variables)]
     let response: Value = client
         .chat()
         .create_byot(json!({
-            "messages": [
-                {
-                    "role": "user",
-                    "content": args.prompt
-                }
-            ],
+            "messages": msgs,
             "model": model,
             "tools": [
                 {
@@ -73,11 +79,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }))
         .await?;
 
-    let resp: LLMResponse = serde_json::from_value(response)?;
-    let tool_calls: Vec<Option<&str>> = resp.choices.iter().map(|c| {
-        extract_filepath(c)
-    })
-    .collect();
+    println!("Response: {:#?}", &response.as_str());
+
+    resp = serde_json::from_value(response)?;
+    
+    if let Some(tcs) = resp
+        .choices
+        .iter()
+        .find_map(|c| c.message.extract_toolcalls())
+    {
+        tool_calls = tcs.clone()
+    } else {
+        break;
+    }
+    
+    } // <- END
     
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     eprintln!("Logs from your program will appear here!");
@@ -86,8 +102,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{}", content);
     }
     
-    if let Some(m) = tool_calls[0] {
-        let fp: FilePath = serde_json::from_str(m)?;
+    if let Some(tc) = tool_calls.first().and_then(|tc| tc.extract_filepath()) {
+        let fp: FilePath = serde_json::from_str(tc)?;
         let wr = fs::read_to_string(fp.file_path.as_str())?;
         println!("{wr}");
     }
@@ -101,13 +117,13 @@ struct FilePath {
     file_path: String
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[allow(unused)]
 struct LLMResponse {
     choices: Vec<Choice>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[allow(unused)]
 struct Choice {
     index: i32,
@@ -115,7 +131,7 @@ struct Choice {
     // finish reason
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 #[allow(unused)]
 struct Message {
     role: String,
@@ -123,7 +139,7 @@ struct Message {
     tool_calls: Option<Vec<ToolCall>>
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[allow(unused)]
 struct ToolCall {
     id: String,
@@ -131,20 +147,37 @@ struct ToolCall {
     function: Function
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[allow(unused)]
 struct Function {
     name: String,
     arguments: String
 }
 
-fn extract_filepath(choice: &Choice) -> Option<&str> {
-    if let Some(tool_calls) = &choice.message.tool_calls {
-        let fct = &tool_calls[0].function;
-        if fct.name == "Read" {
-            return Some(fct.arguments.as_str());
-        }
+impl Message {
+    fn extract_toolcalls(&self) -> Option<&Vec<ToolCall>> {
+        self.tool_calls.as_ref()
     }
-
-    return None;
 }
+
+impl ToolCall {
+    fn extract_filepath(&self) -> Option<&str> {
+        let fct = &self.function;
+        if fct.name.as_str() == "Read" {
+            return Some(&fct.arguments.as_str())
+        }
+
+        None
+    }
+}
+
+// fn extract_filepath(choice: &Choice) -> Option<&str> {
+//     if let Some(tool_calls) = &choice.message.extract_toolcalls() {
+//         let fct = &tool_calls[0].function;
+//         if fct.name == "Read" {
+//             return Some(fct.arguments.as_str());
+//         }
+//     }
+
+//     return None;
+// }
